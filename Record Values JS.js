f90bf012fc996f3a083816bad5dbc3c8a1fe88e2
bbax <john_oliphant@hotmail.com>
2014-12-15 19:37:07 -0500

    Adding my html and js files for conjoint survey

---
+++ Record Values JS.js
@@ -0,0 +1,29 @@
+// Extra Code
+Qualtrics.SurveyEngine.addOnload(function()
+{
+/*Place Your Javascript Below This Line*/
+  this.setChoiceValue(1,pid_a); 
+  this.setChoiceValue(2,care_a);
+  this.setChoiceValue(3,honesty_a);
+  this.setChoiceValue(4,egal_a);
+  this.setChoiceValue(5,authority_a);
+  this.setChoiceValue(6,loyalty_a);
+  this.setChoiceValue(7,purity1_a);
+  this.setChoiceValue(8,purity2_a);
+  this.setChoiceValue(9,competence_a);
+  this.setChoiceValue(10,experience_a);
+  
+  this.setChoiceValue(11,pid_b); 
+  this.setChoiceValue(12,care_b);
+  this.setChoiceValue(13,honesty_b);
+  this.setChoiceValue(14,egal_b);
+  this.setChoiceValue(15,authority_b);
+  this.setChoiceValue(16,loyalty_b);
+  this.setChoiceValue(17,purity1_b);
+  this.setChoiceValue(18,purity2_b);
+  this.setChoiceValue(19,competence_b);
+  this.setChoiceValue(20,experience_b);
+  
+  this.setChoiceValue(21,attributes);
+
+});
